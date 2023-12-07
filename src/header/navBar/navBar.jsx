82a922355e4bb53f64e737e4dac0254c4c8d487a
Antoni Matheus <125527018+antonimatheus@users.noreply.github.com>
2023-12-07 17:18:12 -0400

    start to do the mobile media query

--- src/header/navBar/navBar.jsx
+++ src/header/navBar/navBar.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "/src/assets/assets/shared/logo.svg"
 import "./navBar.css"
 
 function NavBar() {
+
+const [burger, setBurger] = useState(false)
+
+const navBarClicked = () => {
+    setBurger(!burger)
+}
 
     return (
         <div className="NavBar--container">
@@ -17,6 +23,20 @@ function NavBar() {
                     <Link to='/crew'><li><span>02</span> Crew</li></Link>
                     <Link to='/technology'><li><span>03</span> Technology</li></Link>
                 </ul>
+            </div>
+            
+            <div className="NavBar--ulBurger" onClick={navBarClicked}>
+                <img src={burger ? "src/assets/assets/shared/icon-close.svg" : "src/assets/assets/shared/icon-hamburger.svg"} alt="img" />
+                {
+                    burger && (
+                    <ul>
+                        <Link to="/"><li><span>00</span> Home</li></Link>
+                        <Link to="/destination"><li><span>01</span> Destination</li></Link>
+                        <Link to='/crew'><li><span>02</span> Crew</li></Link>
+                        <Link to='/technology'><li><span>03</span> Technology</li></Link>
+                    </ul>  
+                    )
+                }
             </div>
         </div>
     )
