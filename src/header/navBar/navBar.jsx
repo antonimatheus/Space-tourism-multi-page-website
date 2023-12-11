f98abc51b80dd094bd0a62c2a659472987a5861a
Antoni Matheus <125527018+antonimatheus@users.noreply.github.com>
2023-12-11 16:21:15 -0400

    project done!

--- src/header/navBar/navBar.jsx
+++ src/header/navBar/navBar.jsx
@@ -33,10 +33,12 @@ const navBarClicked = () => {
                 {
                     burger && (
                     <ul>
-                        <Link to="/"><li><span>00</span> Home</li></Link>
-                        <Link to="/destination"><li><span>01</span> Destination</li></Link>
-                        <Link to='/crew'><li><span>02</span> Crew</li></Link>
-                        <Link to='/technology'><li><span>03</span> Technology</li></Link>
+                        <div>
+                            <Link to="/"><li><span>00</span> Home</li></Link>
+                            <Link to="/destination"><li><span>01</span> Destination</li></Link>
+                            <Link to='/crew'><li><span>02</span> Crew</li></Link>
+                            <Link to='/technology'><li><span>03</span> Technology</li></Link>
+                        </div>
                     </ul>  
                     )
                 }
