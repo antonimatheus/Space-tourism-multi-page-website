1b398cd5dd0e606782c7e760fa1c4711c5df329b
Antoni Matheus <125527018+antonimatheus@users.noreply.github.com>
2025-03-17 17:39:38 -0400

    updated barrinha

--- src/mainContent/destination/navBarDestinations.jsx
+++ src/mainContent/destination/navBarDestinations.jsx
@@ -1,20 +1,26 @@
 import React from "react";
 import "./Destination.css";
 
-function NavBarDestination({ setSelectedDestination }) {
+function NavBarDestination({ selectedDestination, setSelectedDestination }) {
     return (
         <div className="NavBarDestination--container">
             <div className="NavBarDestination--ul">
                 <ul>
-                    <li onClick={() => setSelectedDestination("Moon")}>Moon</li>
-                    <li onClick={() => setSelectedDestination("Mars")}>Mars</li>
-                    <li onClick={() => setSelectedDestination("Europa")}>Europa</li>
-                    <li onClick={() => setSelectedDestination("Titan")}>Titan</li>
+                    {["Moon", "Mars", "Europa", "Titan"].map((dest) => (
+                        <li 
+                            key={dest} 
+                            onClick={() => setSelectedDestination(dest)}
+                            className={selectedDestination === dest ? "active" : ""}
+                        >
+                            {dest}
+                        </li>
+                    ))}
                 </ul>
             </div>
         </div>
     );
 }
+
 
 export default NavBarDestination;
 
